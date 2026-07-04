2511611b5d1b840964339ed7257bb8dfc2b62f9a
mrali87654 <ali.h.mou97@gmail.com>
2026-07-04 01:48:27 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,16 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
+// Firebase config
+const firebaseConfig = {
     apiKey: "AIzaSyAhNjzfr2PnkeUCEAgPkB5HohpitNSlB78",
     authDomain: "excel-cloud-f0fe2.firebaseapp.com",
     projectId: "excel-cloud-f0fe2",
     storageBucket: "excel-cloud-f0fe2.firebasestorage.app",
     messagingSenderId: "905531285438",
     appId: "1:905531285438:web:e79d37f2a91d5016e1e3ef"
-  };
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+// Initialize Firebase (COMPAT MODE)
+firebase.initializeApp(firebaseConfig);
+
+// create global references
+const auth = firebase.auth();
+const db = firebase.firestore();
